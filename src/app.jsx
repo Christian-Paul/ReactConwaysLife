7c2778e7597260eab83065f96a0ddac815f0f853
Christian Paul <christiansamuelpaul@gmail.com>
2016-08-08 18:57:47 -0400

    Add randomize and clear functions

--- src/app.jsx
+++ src/app.jsx
@@ -2,25 +2,89 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './style.sass';
 
-var Cell = React.createClass({
-	getInitialState: function() {
-		return {status: Math.floor(Math.random()*2)};
-	},
-	toggleStatus: function() {
-		this.setState({status: this.state.status ? 0 : 1});
+var LivingCell = React.createClass({
+	sayPosition: function() {
+		console.log(this.props.x)
+		console.log(this.props.y)
 	},
 	render: function() {
 		return (
-			<div className={'cell' + this.state.status} onClick={this.toggleStatus}></div>
+			<div className={'living-cell'} onClick={this.changeStatus}></div>
+		)
+	}
+});
+
+var DeadCell = React.createClass({
+	sayPosition: function() {
+		console.log(this.props.x)
+		console.log(this.props.y)
+	},
+	render: function() {
+		return (
+			<div className={'dead-cell'}></div>
 		)
 	}
 });
 
 var GameBoard = React.createClass({
+	getInitialState: function() {
+		return {board: [[0, 1, 0], [0, 1, 0], [0, 1, 0]]}
+	},
+	toggleCellStatus: function(x, y) {
+		var board = this.state.board;
+		board[y][x] = board[y][x] ? 0 : 1;
+		this.setState({board: board})
+	},
+	clearBoard: function() {
+		function createArray(H, W) {
+			var arr = [];
+			if(H > 1) {
+				for(var i = 0; i < H; i++) {
+					arr.push(createArray(1, W));
+				};
+			} else {
+				for(var j = 0; j < W; j++) {
+					arr.push(0);
+				}
+			}
+			return arr;
+		};
+		this.setState({board: createArray(10, 10)})
+	},
+	randomizeBoard: function() {
+		function createArrayRandom(H, W) {
+			var arr = [];
+			if(H > 1) {
+				for(var i = 0; i < H; i++) {
+					arr.push(createArrayRandom(1, W));
+				};
+			} else {
+				for(var j = 0; j < W; j++) {
+					arr.push(Math.floor(Math.random()*2));
+				}
+			}
+			return arr;
+		};
+		this.setState({board: createArrayRandom(10, 10)})
+	},
 	render: function() {
 		return (
 			<div className='gameboard'>
-				<Cell />
+				{
+					this.state.board.map(function(subarray, i) {
+						return (
+							<div className='row' key={i}>{subarray.map(function(item, j) {
+								if(item) {
+									return <LivingCell key={j} x={j} y={i} />
+								} else {
+									return <DeadCell key={j} x={j} y={i} />
+								}
+							})}</div>
+						)
+					})
+				}
+				<div onClick={this.randomizeBoard} className='button'>Randomize</div>
+				<div onClick={this.clearBoard} className='button'>Clear</div>
 			</div>
 		)
 	}
